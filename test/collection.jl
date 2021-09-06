787f083289fd507e80c2db79fedeb35b71b5716b
ThomasBreuer <sam@math.rwth-aachen.de>
2021-09-06 23:19:53 +0200

    added functions for exercises
    - `SyllableVector{T}(fpelm::FPGroupElem)`
    - `abelianized(x::SyllableVector{T})`
    
    and added tests for them

--- test/collection.jl
+++ test/collection.jl
@@ -23,8 +23,36 @@
   @test normalform(coll, [(3, 1), (1, 1)]) == [(1, 1), (2, 1), (3, 1)]
 end
 
+@testset "SyllableVector{T}" begin
+  v = [1, -2, 3, -4]
+  sv = SyllableVector{Int}(v)
+  @test sv == [(1, 1), (2, -2), (3, 3), (4, -4)]
+  @test Vector{Int}(sv, 4) == v
+
+  G = small_group(12, 3)
+  pcgs = GAP.Globals.Pcgs(G.X)
+  x = rand(G)
+  y = gens(G)
+  v = SyllableVector{Int}(pcgs, x.X)
+  @test x == prod([y[i]^e for (i, e) in v])
+
+  GG = isomorphic_fp_group(G)[1]
+  x = rand(GG)
+  y = gens(GG)
+  v = SyllableVector{Int}(x)
+  @test x == prod([y[i]^e for (i, e) in v])
+end
+
 @testset "inv" begin
   @test inv(SyllableVector{Int}([1,2,-1])) == SyllableVector{Int}([(3,1),(2,-2),(1,-1)])
+end
+
+@testset "abelianized" begin
+  @test abelianized(SyllableVector{Int}()) == []
+  @test abelianized([(1, 2), (2, 1), (1, -2)]) == [(2, 1)]
+  @test abelianized([(1, 2), (2, -1)]) == [(1, 2), (2, -1)]
+  @test abelianized([(2, -1), (1, 2)]) == [(1, 2), (2, -1)]
+  @test abelianized([(1, 2), (2, -1), (1, -2), (2, 1)]) == []
 end
 
 @testset "collection for random elements in small pc groups" begin
