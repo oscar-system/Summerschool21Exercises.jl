2519ef428f4987aec63b07f9fd541ee95a84a244
Asconik <robert-nowak@online.de>
2021-09-07 10:03:19 +0200

    These are solutions for Julia Exercises 1 to 3. My first commit message! Can you read this? Test test test The quick brown fox jumps over the lazy dog Sometimes when I'm bored I go into the garden and pretend I'm a carrot

---
+++ solutions/Asconik/JuliaSolutions1-3.jl
@@ -0,0 +1,59 @@
+##############################
+#Exercise 1
+##############################
+
+#Function printing hello world
+function hello_world()
+    println("hello world")
+end
+
+function hello(name::String)
+    println("hello $name")
+end
+
+##############################
+#Exercise 2
+##############################
+
+function f(n::Int)
+    if n%2 == 0
+        return div(n,2)
+    else return 3*n+1
+    end
+end
+
+function g(n::Int)
+    k=0
+    while n != 1
+        n = f(n)
+        k += 1
+    end
+    return k
+end
+
+function h()
+    n=101
+    d=Dict{Int,Int}() #Map the k to the n
+        while true
+            k=g(n)
+            if haskey(d, k)
+                return (d[k], n)
+            end
+            d[k]=n
+            n +=1
+        end
+    end
+
+############################################
+#Exercise 3
+############################################
+
+function pascal_triangle(n::Int)
+    for i=0:(n-1)
+        print(" " ^(n-i))
+        for k=0:i
+            print(binomial(i,k), " ")
+        end
+        println(" ")
+    end
+end
